63ea51939ca5850fb4d5b54e447c76a45a8d4b81
DyhaCreator <andropet228@gmail.com>
2023-08-23 11:48:50 -0400

    base i/o and vector tokens

--- main.cpp
+++ main.cpp
@@ -4,19 +4,50 @@
 #include <vector>
 #include "tokens.h"
 
-void parser(){
-
+std::vector<token> parser(std::string code){
+    std::vector<token>parse = std::vector<token>();
+    std::string ACC_SYM = "0123456789+-/*()\n";
+    std::string NUM = "0123456789";
+    std::string ACT = "+-/*";
+    std::string Snum = "";
+    long long LLnum = 0;
+    for(int i = 0; i < code.size(); i++){
+        if(ACC_SYM.find(code[i]) != std::string::npos){
+            if(NUM.find(code[i]) != std::string::npos){
+                Snum += code[i];
+                LLnum = LLnum * 10 + code[i] - '0';
+            }
+            else if(ACT.find(code[i]) != std::string::npos){
+                if(LLnum != 0){
+                    token t(LLnum, Snum, "NUM");
+                    parse.push_back(t);
+                    LLnum = 0;
+                    Snum = "";
+                }
+                token d(code[i], "ACT");
+            }
+            else if(code[i] == '\n' && LLnum != 0){
+                token t(LLnum, Snum, "NUM");
+                parse.push_back(t);
+                LLnum = 0;
+                Snum = "";
+            }
+        }
+    }
+    return parse;
 }
 
 int main(){
-    std::string file, str;
+    std::vector<std::string>file = std::vector<std::string>();
+    std::string str;
     std::ifstream inf("code.in");
     std::cout << "read file ...\n";
     while(std::getline(inf, str)){
-        file += str + "\n";
+        file.push_back(str + '\n');
     }
-    std::cout << file;
+    std::cout << file[0];
     std::cout << "parsing your code ...\n";
-    std::vector<token>n = std::vector<token>();
+    std::vector<token>n = parser(file[0]);
+
     return 0;
 }
--- tokens.h
+++ tokens.h
@@ -1,8 +1,18 @@
 struct token{
+    long long num = 0;
     std::string tok;
     std::string type;
     token(){}
     token(std::string tok, std::string type){
+        this->tok = tok;
+        this->type = type;
+    }
+    token(char tok, std::string type){
+        this->tok = tok;
+        this->type = type;
+    }
+    token(long long num, std::string tok, std::string type){
+        this->num = num;
         this->tok = tok;
         this->type = type;
     }
